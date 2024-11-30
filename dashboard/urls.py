fea7be35a505fb012c362cab290b0b879a900a25
scorsato86murilo <scorsato86murilo@gmail.com>
2024-11-29 22:16:11 -0300

    rotas do dash feito

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
-
+    path('dashboard/painel', views.dashboard, name='dashboard'),
+    path('dashboard/cardapio', views.cardapio, name='cardapio'),
+    path('dashboard/comandas', views.comandas, name='comandas'),
+    path('dashboard/cadastrar_clientes', views.cadastrar_clientes, name='cadastrar_clientes'),
+    path('dashboard/adicionar_produtos', views.adicionar_produtos, name='adicionar_produtos'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
 
+
 def dashboard(request):
     if request.method == 'GET':
         return render(request, 'dashboard.html')
     elif request.method == 'POST':
         return render(request, 'dashboard.html')
+
+
+def comandas(request):
+    if request.method == 'GET':
+        return render(request, 'comandas.html')
+    elif request.method == 'POST':
+        return render(request, 'comandas.html')
+
+
+def cardapio(request):
+    if request.method == 'GET':
+        return render(request, 'cardapio.html')
+    elif request.method == 'POST':
+        return render(request, 'cardapio.html')
+
+
+def cadastrar_clientes(request):
+    if request.method == 'GET':
+        return render(request, 'cadastrar_clientes.html')
+    elif request.method == 'POST':
+        return render(request, 'cadastrar_clientes.html')
+
+
+
+
+
+
+def adicionar_produtos(request):
+    if request.method == 'GET':
+        return render(request, 'adicionar_produtos.html')
+    elif request.method == 'POST':
+        return render(request, 'adicionar_produtos.html')
